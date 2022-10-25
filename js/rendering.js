bc33e58ae8087c58d3a1db4106c5f6235b33d929
wizarlock <shamarorostislav@gmail.com>
2022-10-26 02:26:08 +0300

    module7-task2

--- js/rendering.js
+++ js/rendering.js
@@ -1,5 +1,5 @@
 import {createArrOfPhoto} from './data.js';
-import {viewPhotoInFullSize} from './bigrendering.js';
+import {viewPhotoInFullSize, closeFullSizePhoto, createComments} from './bigrendering.js';
 const photoListElement = document.querySelector('.pictures');
 const photoTemplate = document.querySelector('#picture')
   .content
@@ -9,19 +9,23 @@ export const photos = createArrOfPhoto ();
 
 const photoListFragment = document.createDocumentFragment();
 
-photos.forEach(({url, likes, comments}) => {
+photos.forEach((photo) => {
+  const {url, likes, comments} = photo;
   const photoElement = photoTemplate.cloneNode(true);
   photoElement.querySelector('.picture__img').src = url;
   photoElement.querySelector('.picture__likes').textContent = likes;
   photoElement.querySelector('.picture__comments').textContent = comments.length;
+  photoElement.addEventListener('click', () => {
+    viewPhotoInFullSize(photo);
+    createComments(photo.comments);
+  });
   photoListFragment.appendChild(photoElement);
 });
 
 photoListElement.appendChild(photoListFragment);
-const arr = document.querySelectorAll('.pictures');
-for (let i = 0; i <= arr.length-1; i++) {
-  arr[i].addEventListener('click', () => {
-    viewPhotoInFullSize(photos[i]);
-  });
-}
-
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {closeFullSizePhoto();}
+});
+document.querySelector('.big-picture__cancel').addEventListener('click', () => {
+  closeFullSizePhoto();
+});
