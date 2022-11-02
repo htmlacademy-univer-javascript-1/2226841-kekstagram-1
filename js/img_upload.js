33248ffc81b2f5bf9e3342f9f27fe36792f6df12
wizarlock <shamarorostislav@gmail.com>
2022-11-02 14:12:48 +0300

    module8-task1 part2

--- js/img_upload.js
+++ js/img_upload.js
@@ -1,5 +1,6 @@
 import {isEscapeKey} from './util.js';
 
+const form = document.querySelector('.img-upload__form');
 const classImgUploadOverlay = document.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
 const uploadFile = document.querySelector('#upload-file');
@@ -22,38 +23,62 @@ const inputInBlur = () => {
   document.addEventListener('keydown', onPopupEscKeydown);
 };
 
+function addListeners() {
+  document.addEventListener('keydown', onPopupEscKeydown);
+  document.querySelector('#upload-cancel').addEventListener('click', closeForm);
+  inputForHashtags.addEventListener('focus', inputInFocus);
+  inputForDescription.addEventListener('focus', inputInFocus);
+  inputForHashtags.addEventListener('blur', inputInBlur);
+  inputForDescription.addEventListener('blur', inputInBlur);
+}
+
 function openForm() {
   uploadFile.addEventListener('change', () => {
     classImgUploadOverlay.classList.remove('hidden');
     body.classList.add('modal-open');
-    document.addEventListener('keydown', onPopupEscKeydown);
-    document.querySelector('#upload-cancel').addEventListener('click', closeForm);
-    inputForHashtags.addEventListener('focus', inputInFocus);
-    inputForDescription.addEventListener('focus', inputInFocus);
-    inputForHashtags.addEventListener('blur', inputInBlur);
-    inputForDescription.addEventListener('blur', inputInBlur);
+    addListeners();
   });
+}
+
+function removeListeners() {
+  document.removeEventListener('keydown', onPopupEscKeydown);
+  inputForHashtags.removeEventListener('focus', inputInFocus);
+  inputForDescription.removeEventListener('focus', inputInFocus);
+  inputForHashtags.removeEventListener('blur', inputInBlur);
+  inputForDescription.removeEventListener('blur', inputInBlur);
 }
 
 function closeForm() {
   classImgUploadOverlay.classList.add('hidden');
   body.classList.remove('modal-open');
-  document.addEventListener('keydown', onPopupEscKeydown);
-  inputForHashtags.removeEventListener('focus', inputInFocus);
-  inputForDescription.removeEventListener('focus', inputInFocus);
-  inputForHashtags.removeEventListener('blur', inputInBlur);
-  inputForDescription.removeEventListener('blur', inputInBlur);
+  removeListeners();
   // eslint-disable-next-line no-return-assign
   document.querySelectorAll('input, textarea').forEach((el)=>el.value = '');
 }
 
-const pristine = new Pristine(uploadFile);
+const pristine = new Pristine(form, {
+  classTo: 'text',
+  errorTextParent: 'text'
+});
 
+function isHashtag (hashtag) {
+  const regex = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}/;
+  return regex.test(hashtag);
+}
 
-pristine.addValidator(inputForHashtags, (value) => {
+function validHashtags (value) {
+  if (value === '') {return true;}
   const arrOfHashtags = value.split(' ').map((v) => v.toLowerCase());
+  if ((arrOfHashtags.length > 5) || (!arrOfHashtags.every(isHashtag))) {return false;}
   const duplicates = arrOfHashtags.filter((number, index, numbers) => numbers.indexOf(number) !== index);
-  return duplicates.length > 0;
-}, 'Something Wrong');
+  return duplicates.length === 0;
+}
 
-const valid = pristine.validate();
+pristine.addValidator(inputForHashtags, (value) => validHashtags(value), 'Incorrect hashtags');
+
+form.addEventListener('submit', (evt) => {
+  const isValid = pristine.validate();
+  if (!isValid) {
+    evt.preventDefault();
+  }
+});
