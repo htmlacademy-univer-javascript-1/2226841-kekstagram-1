0982bae78ea32ffd7c101278aa78fe174b13f687
wizarlock <shamarorostislav@gmail.com>
2022-11-09 01:26:56 +0300

    beta

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 import './data.js';
 import './rendering.js';
 import './img_upload.js';
+import { showAlert } from './util.js';
+import { getData } from './api.js';
+import { render } from './rendering.js';
+getData((photos) => {render(photos);}, () => {
+  showAlert('Не удалось загрузить данные. Попробуйте перезагрузить страницу', 5000);
+});
--- js/util.js
+++ js/util.js
@@ -25,3 +25,24 @@ export const isEscapeKey = (evt) => evt.key === 'Escape';
 export function arrayEnded (array) {
   return array.length === 0;
 }
+
+export const showAlert = (message, time) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '30px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, time);
+};
